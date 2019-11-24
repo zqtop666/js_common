677712d238232fe1b0d01badb4824cc30c4f7747
zhangqiang <1702963@qq.com>
2019-11-24 09:24:49 +0800

    图片上传

---
+++ FraUpload 1.1.1/upload.php
@@ -0,0 +1,4 @@
+<?php
+
+header('Content-Type:application/json');//这个类型声明非常关键
+echo '{"code":1,"msg":"操作成功","data":{"id":"407","filepath":"\/uploads\/article\/20190717\/6c25fd31c7821214a991bd9323e1d303.jpg"},"url":"","wait":3}';
